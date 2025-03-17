9767447575f24569c84ae8a961a5b0ad4f7dfbc8
Henry Rothenberg <hsr205@aol.com>
2025-03-17 14:14:47 -0400

    Add endpoint testing logic

--- tests/authorService.test.ts
+++ tests/authorService.test.ts
@@ -1,51 +1,32 @@
-import mongoose, {FilterQuery} from "mongoose";
-import Author, {IAuthor} from "../models/author";
+import mongoose from "mongoose";
+import Author from "../models/author";
+import request from "supertest";
+import app from "../server";
 
 
-// TODO: Complete following test cases
 describe('Verify retrieving author names and lifetimes sorted by author family name', () => {
 
-    const authors = [{}];
+    let consoleSpy: jest.SpyInstance;
 
     beforeAll(() => {
-        Author.find = jest.fn();
+        consoleSpy = jest.spyOn(console, "error").mockImplementation(() => {
+        });
     });
 
     afterAll(() => {
-        jest.clearAllMocks();
+        consoleSpy.mockRestore();
     });
 
-    it("should return 'No authors found' when database is empty", async () => {
-        const expected = authors.map(author => {
-            const a = new Author(author);
-            return `${a.name} : ${a.lifespan}`;
-        });
-        const testExpectation = authors.map(() => {
-            return '';
-        });
-        (Author.find as jest.Mock).mockResolvedValueOnce(authors.map(author => new Author(author)));
-        const result = await Author.getAllAuthors();
-        console.log(`testExpectation = ${testExpectation}`)
-        console.log(`result = ${result}`)
-        expect(testExpectation).toStrictEqual(result);
-    });
+    it("should respond with `No authors found`", async () => {
+        const expectedResponse = "No authors found"
 
-    // test("should return 'No authors found' when database is empty", () => {
-    //     console.log(`Inside test method`)
-    //     const author = new Author({});
-    //     // author.family_name = 'Doe';
-    //     // author.date_of_birth = new Date('1990-01-01');
-    //     // author.date_of_death = new Date('2020-01-01');
-    //     console.log(`author = ${author}`)
-    //     const validationError = author.validateSync();
-    //     console.log(`validationError = ${validationError}`);
-    //     // expect(validationError).toBeDefined();
-    //     // expect(validationError?.errors.first_name).toBeDefined();
-    // });
-    //
-    // test("should return HTTP error code 500 when an error occurs", () => {
-    //
-    // });
+        Author.getAllAuthors = jest.fn().mockResolvedValueOnce(null);
+
+        const response = await request(app).get(`/authors`);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toStrictEqual(expectedResponse);
+    });
 
 
 });
