528660a21fccac3206401dbf0e7b7da05f1280c0
Henry Rothenberg <hsr205@aol.com>
2025-03-17 14:58:03 -0400

    Alter endpoint logic

--- pages/authors.ts
+++ pages/authors.ts
@@ -1,6 +1,5 @@
-import { Response } from 'express';
+import express, {Response} from 'express';
 import Author from '../models/author';
-import express from 'express';
 
 const router = express.Router();
 
@@ -8,21 +7,23 @@ const router = express.Router();
  * @route GET /authors
  * @group Author
  * @returns an array of all authors sorted by family name
- * @returns an error message if no authors were found 
+ * @returns an error message if no authors were found
  * or if there was an error processing the request
  */
 router.get('/', async (_, res: Response) => {
-  try {
-    const data: string[] = await Author.getAllAuthors({ family_name: 1 });
-    if (data.length > 0) {
-      res.send(data);
-    } else {
-      res.send('No authors found');
+    try {
+        console.log("GET /authors endpoint hit");
+
+        const data: string[] | null = await Author.getAllAuthors({ family_name: 1 });
+
+        if (!data || data.length === 0) {
+            return res.send('No authors found');
+        }
+
+        res.send(data);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
     }
-  } catch (error) {
-    console.error('Error processing request:', error);
-    res.send('No authors found');
-  }
 });
 
 export default router;
