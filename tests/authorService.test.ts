e2d7572efc2db9d52aa9c48b9e7873597020238e
Henry Rothenberg <hsr205@aol.com>
2025-03-17 14:57:54 -0400

    Add testing logic

--- tests/authorService.test.ts
+++ tests/authorService.test.ts
@@ -1,10 +1,61 @@
-import mongoose from "mongoose";
 import Author from "../models/author";
 import request from "supertest";
 import app from "../server";
 
 
 describe('Verify retrieving author names and lifetimes sorted by author family name', () => {
+
+    const mockAuthors = [
+        {
+            first_name: 'John',
+            family_name: 'A',
+            date_of_birth: new Date('1900-01-01'),
+            date_of_death: new Date('2020-01-01')
+        },
+        {
+            first_name: 'Jane',
+            family_name: 'B',
+            date_of_birth: new Date('1900-01-01'),
+            date_of_death: new Date('2020-01-01')
+        },
+
+        {
+            first_name: 'Jack',
+            family_name: 'C',
+            date_of_birth: new Date('1900-01-01'),
+            date_of_death: new Date('2020-01-01')
+        },
+        {
+            first_name: 'Jacklyn',
+            family_name: 'D',
+            date_of_birth: new Date('1900-01-01'),
+            date_of_death: new Date('2020-01-01')
+        }
+    ];
+
+    beforeAll(() => {
+        Author.find = jest.fn();
+    });
+
+    afterAll(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should return a list of author names and lifetimes sorted by family name of the authors', async () => {
+        const expected = mockAuthors.map(author => {
+            const a = new Author(author);
+            return `${a.name} : ${a.lifespan}`;
+        });
+        (Author.find as jest.Mock).mockResolvedValueOnce(mockAuthors.map(author => new Author(author)));
+        const result = await Author.getAllAuthors();
+        console.log(result)
+        expect(expected).toStrictEqual(result);
+    });
+
+});
+
+describe('Verify exception are thrown appropriately', () => {
+
 
     let consoleSpy: jest.SpyInstance;
 
@@ -26,6 +77,17 @@ describe('Verify retrieving author names and lifetimes sorted by author family n
 
         expect(response.statusCode).toBe(200);
         expect(response.text).toStrictEqual(expectedResponse);
+    });
+
+    it("should respond with HTTP error code of 500", async () => {
+        const expectedResponse = "Internal Server Error"
+
+        Author.getAllAuthors = jest.fn().mockRejectedValueOnce(new Error("Database error"));
+
+        const response = await request(app).get(`/authors`);
+
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toStrictEqual({error: expectedResponse});
     });
 
 
